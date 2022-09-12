0931e8d7e48cffd0821f2e6b66524aa33cff3649
majbugdol <majbugdol@gmail.com>
2022-09-12 19:53:03 +0200

    add Counter class

--- js/game.js
+++ js/game.js
@@ -1,5 +1,6 @@
 import { Cell } from "./Cell.js";
 import { UI } from "./UI.js";
+import { Counter } from "./Counter.js";
 
 class Game extends UI {
   #config = {
@@ -20,6 +21,8 @@ class Game extends UI {
     },
   };
 
+  #counter = new Counter();
+
   #numberOfRows = null;
   #numberOfCols = null;
   #numberOfMines = null;
@@ -31,6 +34,7 @@ class Game extends UI {
 
   initializeGame() {
     this.#handleElements();
+    this.#counter.init();
     this.#newGame();
   }
 
@@ -46,6 +50,7 @@ class Game extends UI {
     this.#numberOfRows = rows;
     this.#numberOfCols = cols;
     this.#numberOfMines = mines;
+    this.#counter.setValue(this.#numberOfMines);
 
     this.#setStyles();
 
@@ -100,7 +105,16 @@ class Game extends UI {
 
     if (cell.isReveal) return;
 
-    cell.toggleFlag();
+    if (cell.isFlagged) {
+      this.#counter.increment();
+      cell.toggleFlag();
+      return;
+    }
+
+    if (!!this.#counter.value) {
+      this.#counter.decrement();
+      cell.toggleFlag();
+    }
   };
 
   #setStyles() {
