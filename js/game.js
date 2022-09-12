2980f69b0a2ee07048e08cd9e58444f3e0fde607
majbugdol <majbugdol@gmail.com>
2022-09-12 20:43:59 +0200

    add mines and endGame method

--- js/game.js
+++ js/game.js
@@ -25,6 +25,7 @@ class Game extends UI {
   #counter = new Counter();
   #timer = new Timer();
 
+  #isGameFinished = false;
   #numberOfRows = null;
   #numberOfCols = null;
   #numberOfMines = null;
@@ -60,10 +61,20 @@ class Game extends UI {
 
     this.#generateCells();
     this.#renderBoard();
+    this.#placeMinesInCells();
 
     this.#cellsElements = this.getElements(this.UiSelectors.cell);
 
     this.#addCellsEventListeners();
+  }
+
+  #endGame(isWin) {
+    this.#isGameFinished = true;
+    this.#timer.stopTimer();
+
+    if (!isWin) {
+      this.#revealMines();
+    }
   }
 
   #generateCells() {
@@ -90,14 +101,31 @@ class Game extends UI {
     });
   }
 
+  #placeMinesInCells() {
+    let minesToPlace = this.#numberOfMines;
+
+    while (minesToPlace) {
+      const rowIndex = this.#getRandomInteger(0, this.#numberOfRows - 1);
+      const colIndex = this.#getRandomInteger(0, this.#numberOfCols - 1);
+
+      const cell = this.#cells[rowIndex][colIndex];
+
+      const hasCellMine = cell.isMine;
+
+      if (!hasCellMine) {
+        cell.addMine();
+        minesToPlace--;
+      }
+    }
+  }
+
   #handleCellClick = (e) => {
     const target = e.target;
     const rowIndex = parseInt(target.getAttribute("data-y"), 10);
     const colIndex = parseInt(target.getAttribute("data-x"), 10);
 
     const cell = this.#cells[rowIndex][colIndex];
-    if (cell.isFlagged) return;
-    cell.revealCell();
+    this.#clickCell(cell);
   };
 
   #handleCellContextMenu = (e) => {
@@ -109,7 +137,7 @@ class Game extends UI {
 
     const cell = this.#cells[rowIndex][colIndex];
 
-    if (cell.isReveal) return;
+    if (cell.isReveal || this.#isGameFinished) return;
 
     if (cell.isFlagged) {
       this.#counter.increment();
@@ -123,11 +151,32 @@ class Game extends UI {
     }
   };
 
+  #clickCell(cell) {
+    if (this.#isGameFinished || cell.isFlagged) return;
+
+    if (cell.isMine) {
+      this.#endGame(false);
+    }
+    cell.revealCell();
+  }
+
+  #revealMines() {
+    this.#cells
+      .flat()
+      .filter(({ isMine }) => isMine)
+      .forEach((cell) => cell.revealCell());
+    //{} destrukturyzacja - zamiast (cell) => cell.isMine
+  }
+
   #setStyles() {
     document.documentElement.style.setProperty(
       "--cells-in-row",
       this.#numberOfCols
     );
+  }
+
+  #getRandomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
   }
 }
 
--- readme.md
+++ readme.md
@@ -1,5 +1,9 @@
 # Projekt
 
+Projekt przedstawia znaną jako nieodłaczny dodatek do systemu Windows grę Saper.
+Gra posiada trzy poziomy trudności różniące się wielkością planszy i ilością min.
+Limit czasowy gry to 999 sekund.
+
 # Źródło
 
 Podstawy funkcjonalności na bazie kursu Michała Dziedzińskiego
